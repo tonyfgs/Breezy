976113c5f2e5c6e6a799af8ac44d6564ad6f8aba
Robin Noiret <119452365+RobinNoiret@users.noreply.github.com>
2026-06-12 10:10:12 +0200

    fix: clear old js files

--- microservices/iam/src/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 4001;
-
-app.listen(PORT, () => {
-  console.log(`IAM service running on port ${PORT}`);
-});

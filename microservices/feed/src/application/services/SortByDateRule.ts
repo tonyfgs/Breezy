052959cb82b52ef3dce63fa8776eecd098e36d0e
tonyfages <tony.fages@etu.uca.fr>
2026-06-22 10:17:26 +0200

    feat(feed): implement GetFeedUseCase and update FeedController for fetching user feeds

--- microservices/feed/src/application/services/SortByDateRule.ts
+++ microservices/feed/src/application/services/SortByDateRule.ts
@@ -3,6 +3,6 @@ import {PostEntity} from "../../domain/entities/PostEntity";
 
 export class SortByDateRule implements ISortingRule {
     sort(posts: PostEntity[]): PostEntity[] {
-        return [...posts].sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+        return [...posts].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
     }
 }
